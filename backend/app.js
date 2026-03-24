2e56593e05f3d6975e3e8fe90e0803ca90b4dd95
Nikko <atuan.nikko@gmail.com>
2026-03-24 22:18:56 +0800

    feat: added mongodb and model

--- backend/app.js
+++ backend/app.js
@@ -1,5 +1,8 @@
+require('dotenv').config();
+
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
 const feedRoutes = require('./routes/feed');
 
@@ -17,4 +20,9 @@ app.use((req, res, next) => {
 
 app.use('/feed', feedRoutes);
 
-app.listen(8080);
+mongoose.connect(process.env.MONGODB_URI)
+    .then(() => {
+        app.listen(8080, () => console.log('Server running on port 8080'));
+    })
+    .catch(err => console.log(err));
+
