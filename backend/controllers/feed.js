cb5bbcfb87a064a0968cd4b49ae722cdf8c34478
Nikko <atuan.nikko@gmail.com>
2026-03-24 21:59:12 +0800

    feat: added fetch post

--- backend/controllers/feed.js
+++ backend/controllers/feed.js
@@ -1,6 +1,17 @@
 exports.getPosts = (req, res, next) => {
   res.status(200).json({
-    posts: [{ title: 'First Post', content: 'This is the first post!' }]
+    posts: [
+      { 
+        _id: 'test',
+        title: 'First Post', 
+        content: 'This is the first post!', 
+        imageUrl: '',
+        creator: {
+          name: 'Test'
+        },
+        date: new Date()
+      }
+    ]
   });
 };
 
