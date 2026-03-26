8f9e1e182c683444bfe6fee0aac02449b62933d7
Nikko <atuan.nikko@gmail.com>
2026-03-26 22:33:53 +0800

    feat: updated createPost API

--- backend/controllers/feed.js
+++ backend/controllers/feed.js
@@ -1,5 +1,7 @@
 const { validationResult } = require('express-validator');
 
+const Post = require('../models/post');
+
 exports.getPosts = (req, res, next) => {
   res.status(200).json({
     posts: [
@@ -30,17 +32,21 @@ exports.createPost = (req, res, next) => {
 
   const title = req.body.title;
   const content = req.body.content;
-  // Create post in db
-  res.status(201).json({
-    message: 'Post created successfully!',
-    post: { 
-      id: new Date().toISOString(), 
-      title: title, 
-      content: content,
-      creator: {
-        name: 'Test'
-      },
-      createdAt: new Date()
+  const post = new Post({
+    title: title,
+    content: content,
+    imageUrl: 'test',
+    creator: {
+      name: 'Test'
     }
   });
+  post.save()
+    .then(result=> {
+      console.log(result);
+      res.status(201).json({
+        message: 'Post created successfully!',
+        post: result
+      });
+    })
+    .catch(err => console.log(err));  
 };
