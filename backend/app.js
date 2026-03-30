fe8f73113bc2c96ae10a4ce775f5b18aa6799e59
Nikko <atuan.nikko@gmail.com>
2026-03-30 22:15:15 +0800

    feat: socket io setup

--- backend/app.js
+++ backend/app.js
@@ -56,7 +56,7 @@ mongoose.connect(process.env.MONGODB_URI)
     .then(() => {
         const server = app.listen(8080, () => console.log('Server running on port 8080'));
 
-        const io = require('socket.io')(server);
+        const io = require('./socket').init(server);
         io.on('connection', socket => {
             console.log('Client connected');
         });
