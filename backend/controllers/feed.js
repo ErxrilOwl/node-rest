69395c2011b4cb123230a44836e0764f4fc858a9
Nikko <atuan.nikko@gmail.com>
2026-03-30 22:27:45 +0800

    feat: added update event listener

--- backend/controllers/feed.js
+++ backend/controllers/feed.js
@@ -66,7 +66,13 @@ exports.createPost = async (req, res, next) => {
 
     io.getIO().emit('posts', {
       action: 'create',
-      post: post
+      post: {
+        ...post._doc,
+        creator: {
+          _id: req.userId,
+          name: user.name
+        }
+      }
     });
 
     res.status(201).json({
@@ -130,7 +136,7 @@ exports.updatePost = async (req, res, next) => {
   }
 
   try {
-    const post = await Post.findById(postId);
+    const post = await Post.findById(postId).populate('creator');
 
     if (!post) {
       const error = new Error("Could not find post");
@@ -138,7 +144,7 @@ exports.updatePost = async (req, res, next) => {
       throw error;
     }
 
-    if (post.creator.toString() !== req.userId) {
+    if (post.creator._id.toString() !== req.userId) {
       const error = new Error('Not authorized!');
       error.statusCode = 403;
       throw error;
@@ -153,6 +159,11 @@ exports.updatePost = async (req, res, next) => {
     post.content = content;
 
     const result = await post.save();
+
+    io.getIO().emit('posts', {
+      action: 'update',
+      post: result
+    })
 
     res.status(200).json({
       message: "Post updated!",
